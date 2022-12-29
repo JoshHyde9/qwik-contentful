c101a9f688e24932d8d8a7af86964be6edee0122
JoshHyde9 <joshhyde546@gmail.com>
2022-12-30 10:34:31 +1100

    feat: added card for blog

---
+++ src/components/BlogCard.tsx
@@ -0,0 +1,23 @@
+import { component$ } from "@builder.io/qwik";
+import dayjs from "dayjs";
+
+type CardProps = {
+  title: string;
+  description: string;
+  publishedDate: string;
+  readingTime: number;
+};
+
+export default component$<CardProps>(
+  ({ title, description, publishedDate, readingTime }) => (
+    <div class="flex flex-col gap-6 my-5 p-5 rounded-xl hover:bg-gray-100 transition-colors duration-200">
+      <h2 class="text-2xl font-semibold">{title}</h2>
+      <p>{description}</p>
+      <div class="flex gap-2">
+        <p>{dayjs(publishedDate).format("MMMM DD, YYYY")}</p>
+        <p>&#x2022;</p>
+        <p>{readingTime} min read</p>
+      </div>
+    </div>
+  )
+);
