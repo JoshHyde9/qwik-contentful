31e70adbc80de3517c4295abacedc3f2d93e3871
JoshHyde9 <joshhyde546@gmail.com>
2022-12-30 10:32:51 +1100

    feat: added func to get all contentful entries

--- src/service/contentful.ts
+++ src/service/contentful.ts
@@ -1,4 +1,4 @@
-import type { Entry } from "contentful";
+import type { Entry, EntryCollection } from "contentful";
 
 export const getEntry = async <T>(slug: string) => {
   const request = `https://cdn.contentful.com/spaces/${
@@ -18,6 +18,24 @@ export const getEntry = async <T>(slug: string) => {
   if (data.sys.id === "NotFound") {
     return null;
   }
+
+  return data;
+};
+
+export const getEntries = async <T>() => {
+  const request = `https://cdn.contentful.com/spaces/${
+    import.meta.env.VITE_CONTENTFUL_SPACE_ID
+  }/environments/master/entries/?access_token=${
+    import.meta.env.VITE_CONTENTFUL_API_KEY
+  }`;
+
+  const response = await fetch(request, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data: EntryCollection<T> = await response.json();
 
   return data;
 };
