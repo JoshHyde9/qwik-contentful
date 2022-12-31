c6bf7bb1665d040087238c295547b6f276cdd56a
JoshHyde9 <joshhyde546@gmail.com>
2023-01-01 09:05:28 +1100

    feat: added custom faivicon

--- src/components/router-head.tsx
+++ src/components/router-head.tsx
@@ -1,5 +1,5 @@
-import { component$ } from '@builder.io/qwik';
-import { useDocumentHead, useLocation } from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
 
 /**
  * The RouterHead component is placed inside of the document `<head>` element.
@@ -14,7 +14,7 @@ export const RouterHead = component$(() => {
 
       <link rel="canonical" href={loc.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.png" />
 
       {head.meta.map((m) => (
         <meta {...m} />
