2bb53139a838f0b7052592f5a2ffd8f665626f5a
JoshHyde9 <joshhyde546@gmail.com>
2023-01-01 08:16:31 +1100

    feat: started about page

---
+++ src/routes/about/index.tsx
@@ -0,0 +1,24 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <div class="container mx-auto flex flex-col gap-10 px-5 lg:flex-row">
+      <div>
+        <img src="/images/me.jpg" class="block w-full" alt="Ryan Brooks" />
+      </div>
+      <div>
+        <h1 class="text-4xl font-semibold text-coral mb-5">Hello, I'm Ryan</h1>
+        <p>
+          As an IT career professional, I have had experience in the technical
+          and operational spaces. During my secondary and tertiary education, I
+          constantly focused on ICT and improving my knowledge from my in school
+          experience. Having established myself in the industry, I am continuing
+          to develop and improve my skills as I look for further opportunities.
+          My continually expanding knowledge is enhanced by my home
+          infrastructure, including networking equiptment, server hardware and
+          configuration.
+        </p>
+      </div>
+    </div>
+  );
+});
