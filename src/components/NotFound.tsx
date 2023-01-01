1f054aa2ccb2ae2c49555eea447af7006b2fdca4
JoshHyde9 <joshhyde546@gmail.com>
2023-01-02 10:43:45 +1100

    fix: better design for 404

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -2,14 +2,17 @@ import { component$ } from "@builder.io/qwik";
 
 export const NotFound = component$(() => {
   return (
-    <div class="w-screen h-screen text-center bg-slate-900 text-black flex flex-col justify-center items-center">
-      <div class="flex flex-col align-center justify-center rounded-full bg-yellow-400 h-1/2">
+    <div class="w-screen h-[calc(100vh_-_80px)] flex flex-col justify-center items-center text-center">
+      <div class="flex flex-col align-center justify-center">
         <h1 class="text-9xl">404</h1>
-        <p class="p-2">
+        <p class="text-lg p-2">
           Nothing to see here.... No, literally, nothing to see here!
         </p>
         <div class="mt-5 lg:mt-10">
-          <a href="/" class="bg-blue-700 py-2.5 px-5 rounded-full text-white">
+          <a
+            href="/"
+            class="bg-coral py-2.5 px-5 rounded-full text-white duration-150 hover:bg-coral/75"
+          >
             Go back home
           </a>
         </div>
