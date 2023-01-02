0240054f01c93e2f7f806b7e400c584bff33e835
JoshHyde9 <joshhyde546@gmail.com>
2023-01-03 10:32:33 +1100

    feat: changed layout for about page

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -3,12 +3,19 @@ import { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
-    <div class="container mx-auto flex flex-col gap-10 px-5 lg:flex-row">
-      <div>
+    <div class="container flex flex-col items-center mx-auto max-w-7xl lg:flex-row lg:items-center">
+      <div class="max-w-xl">
         <img src="/images/me.jpg" class="block w-full" alt="Ryan Brooks" />
       </div>
-      <div>
-        <h1 class="text-4xl font-semibold text-coral mb-5">Hello, I'm Ryan</h1>
+      <div class="bg-slate-50 lg:-ml-16 lg:mt-[33%] p-4 pl-10">
+        <div class="max-w-lg pt-6">
+          <h1 class="text-4xl font-semibold mb-5">Hello There!</h1>
+          <h2 class="text-4xl font-semibold mb-5 lg:mb-24">
+            My name is Ryan. I am a full-time{" "}
+            <span class="text-coral">virgin</span> and{" "}
+            <span class="text-coral">sysadmin</span>.
+          </h2>
+        </div>
         <p>
           As an IT career professional, I have had experience in the technical
           and operational spaces. During my secondary and tertiary education, I
