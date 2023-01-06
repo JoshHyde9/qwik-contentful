b8d0fa9f69c9043fbc78106711f5385b87db436c
JoshHyde9 <joshhyde546@gmail.com>
2023-01-06 20:31:49 +1100

    feat: added loading state to email request

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -1,4 +1,10 @@
-import { component$, $, useStore, QwikChangeEvent } from "@builder.io/qwik";
+import {
+  component$,
+  $,
+  useStore,
+  QwikChangeEvent,
+  useSignal,
+} from "@builder.io/qwik";
 import { DocumentHead } from "@builder.io/qwik-city";
 import { ZodError } from "zod";
 
@@ -8,6 +14,7 @@ import { type EmailData, emailSchema } from "~/util/schema";
 export default component$(() => {
   const store = useStore<EmailData>({ name: "", email: "", message: "" });
   const responseStore = useStore({ message: "" });
+  const loading = useSignal(false);
 
   const onChange = $(
     (e: QwikChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -16,10 +23,12 @@ export default component$(() => {
   );
 
   const onSubmit = $(async () => {
+    loading.value = true;
     try {
       emailSchema.parse(store);
     } catch (err) {
       if (err instanceof ZodError) {
+        loading.value = false;
         return (responseStore.message = err.issues[0].message);
       }
     }
@@ -30,6 +39,7 @@ export default component$(() => {
     });
 
     const data = await response.json();
+    loading.value = false;
 
     if (data.errors) {
       responseStore.message = data.errors.description;
@@ -42,17 +52,25 @@ export default component$(() => {
   });
 
   return (
-    <div class="container mx-auto max-w-[65ch]">
-      <h1>Contact me</h1>
+    <div class="container mx-auto max-w-[65ch] px-2 md:px-0">
+      <h1 class="text-4xl font-bold mb-1">Contact me</h1>
+      <p>
+        Do you have any questions, concerns or just want to say g'day? Just
+        chuck me an email.
+      </p>
 
-      <form preventdefault:submit class="flex flex-col" onSubmit$={onSubmit}>
+      <form
+        preventdefault:submit
+        class="flex flex-col mt-5"
+        onSubmit$={onSubmit}
+      >
         <div class="flex flex-col my-4">
           <FormField
             label="Name:"
             element="input"
             type="text"
             name="name"
-            placeholder="Your name"
+            placeholder="John Doe"
             value={store.name}
             onChange={onChange}
           />
@@ -74,19 +92,43 @@ export default component$(() => {
             element="textarea"
             rows={4}
             name="message"
-            placeholder="Your message here..."
+            placeholder="I love your website!"
             value={store.message}
             onChange={onChange}
           />
         </div>
 
-        {responseStore.message && <p class="mb-4">{responseStore.message}</p>}
+        {responseStore.message && (
+          <p class="italic text-sm mb-4">{responseStore.message}</p>
+        )}
 
         <button
           type="submit"
-          class="bg-coral py-2 rounded-md duration-300 text-white hover:bg-coral/75"
+          disabled={loading.value}
+          class="w-full flex justify-center bg-coral py-2 rounded-md duration-300 text-white hover:bg-coral/75 disabled:cursor-not-allowed"
         >
-          Send email
+          {loading.value ? (
+            <svg
+              class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                class="opacity-100 stroke-2"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+              ></circle>
+              <path
+                class="fill-coral"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          ) : (
+            "Send email"
+          )}
         </button>
       </form>
     </div>
