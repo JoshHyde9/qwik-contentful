a0f74ca8a89678b43c3fc97f9b590dc33e92006a
JoshHyde9 <joshhyde546@gmail.com>
2023-01-07 13:00:38 +1100

    fix: styling for small mobile devices

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -11,7 +11,7 @@ type CardProps = {
 
 export const BlogCard = component$<CardProps>(
   ({ title, description, publishedDate, readingTime, previewImage }) => (
-    <div class="flex flex-row justify-between items-center gap-6 my-5 p-5 rounded-xl transition-colors duration-300 hover:bg-global-warming/25 dark:hover:bg-neutral-600/25">
+    <div class="flex flex-row justify-between items-center md:gap-6 my-5 py-5 px-2 rounded-xl transition-colors duration-300 md:px-5 hover:bg-global-warming/25 dark:hover:bg-neutral-600/25">
       <div class="flex flex-col gap-6">
         <h2 class="text-2xl font-semibold">{title}</h2>
         <p>{description}</p>
@@ -21,7 +21,7 @@ export const BlogCard = component$<CardProps>(
           <p>{readingTime} min read</p>
         </div>
       </div>
-      <div class="w-1/3">
+      <div class="w-1/4 md:w-1/3">
         <img class="rounded-md" src={previewImage} alt="Preview image" />
       </div>
     </div>
