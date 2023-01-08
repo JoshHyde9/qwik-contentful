1edcb014f589ad692b32f29b2eaf9cb277c676aa
JoshHyde9 <joshhyde546@gmail.com>
2023-01-08 14:26:44 +1100

    fix: error messages are more obvious

--- src/util/schema.ts
+++ src/util/schema.ts
@@ -3,11 +3,11 @@ import { z } from "zod";
 export const emailSchema = z.object({
   name: z
     .string()
-    .min(1, { message: "Name must contain at least 1 character(s)" }),
+    .min(1, { message: "Name must not be empty" }),
   email: z.string().email(),
   message: z
     .string()
-    .min(1, { message: "Message must contain at least 1 character(s)" }),
+    .min(1, { message: "Message must not be empty" }),
 });
 
 export type EmailData = z.infer<typeof emailSchema>;
