76321bd7bfbc635602ef1a26543ec901da95d9ba
JoshHyde9 <joshhyde546@gmail.com>
2023-01-09 11:37:45 +1100

    feat: created small icon lib

---
+++ src/components/Icon.tsx
@@ -0,0 +1,18 @@
+import { component$ } from "@builder.io/qwik";
+import { IconNames, Icons } from "~/util/iconLib";
+
+type IconProperties = {
+  class?: string;
+  viewBox?: string;
+  style?: any;
+  size?: "16" | "24" | "32" | "40";
+  name: IconNames;
+};
+
+export const Icon = component$<IconProperties>(
+  ({ viewBox = "0 0 24 24", size = "24", name, ...props }) => (
+    <svg width={size} height={size} viewBox={viewBox} {...props}>
+      {Icons[name]()}
+    </svg>
+  )
+);
