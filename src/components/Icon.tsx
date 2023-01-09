3d6f42832af89380cdc20d2facfc93bd4ab97a26
JoshHyde9 <joshhyde546@gmail.com>
2023-01-09 11:38:50 +1100

    fix: changed type name to props

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,7 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 import { IconNames, Icons } from "~/util/iconLib";
 
-type IconProperties = {
+type IconProps = {
   class?: string;
   viewBox?: string;
   style?: any;
@@ -9,7 +9,7 @@ type IconProperties = {
   name: IconNames;
 };
 
-export const Icon = component$<IconProperties>(
+export const Icon = component$<IconProps>(
   ({ viewBox = "0 0 24 24", size = "24", name, ...props }) => (
     <svg width={size} height={size} viewBox={viewBox} {...props}>
       {Icons[name]()}
