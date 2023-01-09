237c70e8ca9803f8e12eac7731188df3059655a5
JoshHyde9 <joshhyde546@gmail.com>
2023-01-09 11:16:14 +1100

    feat: added coral colour to card headings

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -13,7 +13,7 @@ export const BlogCard = component$<CardProps>(
   ({ title, description, publishedDate, readingTime, previewImage }) => (
     <div class="flex flex-row justify-between items-center my-5 py-5 px-1 rounded-xl transition-colors duration-300 md:px-5 md:gap-6 hover:bg-global-warming/25 dark:hover:bg-neutral-600/25">
       <div class="flex flex-col max-w-[240px] gap-y-6 md:max-w-xs">
-        <h2 class="text-2xl font-semibold">{title}</h2>
+        <h2 class="text-coral text-2xl font-semibold">{title}</h2>
         <p>{description}</p>
         <div class="flex gap-2">
           <p>{dayjs(publishedDate).format("MMMM DD, YYYY")}</p>
