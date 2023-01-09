9b6ee71a22e955b605810a382e056ed0f1a997c1
JoshHyde9 <joshhyde546@gmail.com>
2023-01-09 11:38:13 +1100

    feat: added icons to info

--- src/routes/(pages)/blog/[slug]/index.tsx
+++ src/routes/(pages)/blog/[slug]/index.tsx
@@ -12,6 +12,7 @@ import { getEntryBySlug } from "~/service/contentful";
 
 import { calcReadingTime } from "~/util/calcReadingTime";
 import { NotFound } from "~/components/NotFound";
+import { Icon } from "~/components/Icon";
 
 export const onGet: RequestHandler<Entry<BlogData> | null> = async ({
   params,
@@ -34,10 +35,18 @@ export default component$(() => {
             <NotFound />
           ) : (
             <article class="container mx-auto max-w-prose py-10 px-2 md:px-0">
-              <h1 class="text-4xl font-bold">{post.fields.title}</h1>
+              <h1 class="text-4xl font-bold mb-5">{post.fields.title}</h1>
 
-              <p>{dayjs(post.sys.createdAt).format("MMMM DD, YYYY")}</p>
-              <p>{calcReadingTime(post.fields.content)} min read</p>
+              <div class="flex items-center">
+                <Icon name="Calendar" class="stroke-white fill-none" />
+                <p>{dayjs(post.sys.createdAt).format("MMMM DD, YYYY")}</p>
+              </div>
+
+              <div class="flex items-center">
+                <Icon name="Clock" class="stroke-white fill-none" />
+                <p>{calcReadingTime(post.fields.content)} min read</p>
+              </div>
+
               <hr class="my-5" />
               <div
                 class="blog-styles"
