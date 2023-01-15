33779995d79243ba7eec537272fc764deb4280be
JoshHyde9 <joshhyde546@gmail.com>
2023-01-15 13:54:47 +1100

    fix: layout stuff

--- src/routes/(pages)/layout!.tsx
+++ src/routes/(pages)/layout!.tsx
@@ -4,7 +4,7 @@ import { Navbar } from "~/components/layout/Navbar";
 export default component$(() => {
   return (
     <>
-      <main class="min-h-screen dark:bg-neutral-800">
+      <main class="min-h-screen leading-6 dark:bg-neutral-800 lg:text-lg">
         <Navbar />
         <section class="h-[calc(100%_-_80px)] dark:bg-neutral-800">
           <Slot />
