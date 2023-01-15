e8c8517fce095e8d36847fb46af6dc06e6ebf06f
JoshHyde9 <joshhyde546@gmail.com>
2023-01-15 14:00:24 +1100

    hotfix: AAAAAAAAAAAAAA

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -4,7 +4,7 @@ import { Navbar } from "~/components/layout/Navbar";
 export default component$(() => {
   return (
     <>
-      <main class="h-screen">
+      <main class="h-screen lg:text-lg">
         <Navbar />
         <section class="h-[calc(100%_-_80px)]">
           <Slot />
